ea2a33d11fe4b54cbc054c1971d333c64e3fda7b
Alex Nugent <aonugent@gmail.com>
2023-08-12 16:41:13 -0400

    add secrets manager retrieval support

---
+++ src/main/java/com/an/weatherappservice/client/CredentialRetriever.java
@@ -0,0 +1,46 @@
+package com.an.weatherappservice;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
+
+public class CredentialRetriever {
+    public String getSecret() throws JsonProcessingException {
+
+        String secretName = "openWeatherApiKey";
+        Region region = Region.of("us-east-1");
+
+        // Create a Secrets Manager client
+        SecretsManagerClient client = SecretsManagerClient.builder()
+                .region(region)
+                .build();
+
+        GetSecretValueRequest getSecretValueRequest = GetSecretValueRequest.builder()
+                .secretId(secretName)
+                .build();
+
+        GetSecretValueResponse getSecretValueResponse;
+
+        try {
+            getSecretValueResponse = client.getSecretValue(getSecretValueRequest);
+        } catch (Exception e) {
+            // For a list of exceptions thrown, see
+            // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+            throw e;
+        }
+
+        String secretString = getSecretValueResponse.secretString();
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(secretString);
+        JsonNode apiKey = root.path("openWeatherApiKey");
+        return apiKey.toString().replaceAll("\"","" );
+
+        // Your code goes here.
+    }
+}
